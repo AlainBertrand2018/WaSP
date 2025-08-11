6317cc15650e017c8ccfb170f01a0a983dcdc705
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-08-11 05:19:02 +0000

    as part of the public-facing website, create a new page ./sme-info where

---
+++ src/app/sme-info/page.tsx
@@ -0,0 +1,23 @@
+
+'use client';
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function SmeInfoPage() {
+    return (
+        <div className="flex flex-col flex-1 w-full">
+            <Card className="w-full flex-1 flex flex-col">
+                <CardHeader>
+                    <CardTitle className="text-3xl">SME & Startup Dashboard for Mauritius</CardTitle>
+                </CardHeader>
+                <CardContent className="flex-1 flex flex-col">
+                    <iframe
+                        src="/documents/mauritius_sme_startup_dashboard.html"
+                        title="SME & Startup Dashboard for Mauritius"
+                        className="w-full h-full flex-1 border-0"
+                    />
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
