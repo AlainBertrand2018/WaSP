8ad1ff5c990bd3d02f062087babb35ed8d421594
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-08-12 20:08:41 +0000

    Make the container 20% taller

--- src/components/feature/chatbot.tsx
+++ src/components/feature/chatbot.tsx
@@ -80,7 +80,7 @@ export default function Chatbot() {
 
   return (
     <Card className={cn(
-        "fixed z-50 h-[600px] flex flex-col shadow-lg bg-secondary rounded-lg",
+        "fixed z-50 h-[720px] flex flex-col shadow-lg bg-secondary rounded-lg",
         "w-[90%] bottom-4 left-1/2 -translate-x-1/2",
         "sm:w-full sm:max-w-sm sm:bottom-6 sm:right-6 sm:left-auto sm:translate-x-0"
     )}>
