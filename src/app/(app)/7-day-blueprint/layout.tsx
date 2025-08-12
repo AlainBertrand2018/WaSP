305397aad08d9a4d26bfe284db0cce7162dd8a3c
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-08-12 16:36:50 +0000

    For the 7-day-blueprint suite it seems that cards flush off on the right

--- src/app/(app)/7-day-blueprint/layout.tsx
+++ src/app/(app)/7-day-blueprint/layout.tsx
@@ -11,7 +11,7 @@ export default function SevenDayBlueprintLayout({ children }: { children: React.
       <SevenDayBlueprintSidebarNav />
       <SidebarInset>
         <div className="flex flex-col">
-          <main className="flex-1 p-6 sm:p-8">
+          <main className="flex-1 p-4">
             {children}
           </main>
         </div>
