7ff326a62d43c1f86bdbfde488fc9fda26e046d0
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-08-13 00:45:48 +0000

    Oh do not forget to comment the 48-hr no display component too

--- src/components/feature/claire-intro-popup.tsx
+++ src/components/feature/claire-intro-popup.tsx
@@ -18,6 +18,7 @@ export function ClaireIntroPopup() {
   const { openPlayer } = useAudioPlayerStore();
 
   useEffect(() => {
+    /*
     const lastSeen = localStorage.getItem(POPUP_STORAGE_KEY);
     const now = Date.now();
 
@@ -25,11 +26,12 @@ export function ClaireIntroPopup() {
       // If seen within the last 48 hours, don't show the popup.
       return;
     }
+    */
 
     // Show the popup after a 1-second delay.
     const showTimer = setTimeout(() => {
       setIsOpen(true);
-      localStorage.setItem(POPUP_STORAGE_KEY, now.toString());
+      localStorage.setItem(POPUP_STORAGE_KEY, Date.now().toString());
     }, 1000);
 
     // Set a timer to automatically close the popup after 12 seconds if not interacted with.
