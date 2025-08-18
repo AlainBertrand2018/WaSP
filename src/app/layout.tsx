831909356cf4fdadf94b249334c2dfc7972c6f2a
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-08-18 21:45:51 +0000

    App seems to ignore the original css (poppins is not being used.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 
 import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/layout/theme-provider';
+import { cn } from '@/lib/utils';
+
+const fontSans = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans',
+});
 
 // A template title for other pages in the app
 export const metadata: Metadata = {
@@ -19,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
