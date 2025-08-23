e72b20bab5fa0b6219c0839987cf478999a8b445
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-08-23 06:35:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,34 @@
+
+require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env') });
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tgapgvvufswaxsyyhnna.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_HYPERADMIN_EMAIL: process.env.HYPERADMIN_EMAIL,
+  },
+};
+
+export default nextConfig;
